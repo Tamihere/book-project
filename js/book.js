22409e103333a6ccbe7aebcda93b8abed6b6ff0f
Tamihere <Toka@Tamihere.com>
2019-10-25 01:19:28 +1300

    updated media queries and favicons

--- js/book.js
+++ js/book.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
-  //*/ All elements with the "draggy" class become draggable//*
-  $('.draggy').draggable();
+$(function() {
+  function init() {
+    // Toggle bg colour on and off
+    $('.js-tk-position').on('click', function (e) {
+      e.preventDefault();
+      $('body').toggleClass('tk-reveal--on');
+    })
+  }
+  
+  init();
 });
